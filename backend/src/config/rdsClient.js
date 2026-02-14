f44b37f89313d4bc49a57d9a6b1fc85d64c70eb1
abhinav <abhinavpathak9984@gmail.com>
2026-02-14 15:16:28 +0530

    fix: Redis client configuration, and time formatting utilities.

--- backend/src/config/rdsClient.js
+++ backend/src/config/rdsClient.js
@@ -1,45 +1,34 @@
-import {Redis} from "ioredis";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-let isProd = process.env.NODE_ENV === "production";
-
 let redisClient;
 
-if(isProd){
+async function initRedis() {
+  if (
+    process.env.UPSTASH_REDIS_REST_URL &&
+    process.env.UPSTASH_REDIS_REST_TOKEN
+  ) {
     const { Redis } = await import("@upstash/redis");
 
     redisClient = new Redis({
-        url: process.env.UPSTASH_REDIS_REST_URL,
-        token: process.env.UPSTASH_REDIS_REST_TOKEN,
+      url: process.env.UPSTASH_REDIS_REST_URL,
+      token: process.env.UPSTASH_REDIS_REST_TOKEN,
     });
+
+    console.log("Using Upstash Redis (Production)");
+  }
+
+  else {
+    const IoRedis = (await import("ioredis")).default;
+
+    redisClient = new IoRedis({
+      host: "127.0.0.1",
+      port: 6379,
+    });
+
+    console.log("Using Local Redis (Docker)");
+  }
+
+  return redisClient;
 }
 
-console.log("isprod: ", isProd);
+const redis = await initRedis();
 
-if(!isProd){
-    
-//     redisClient = new Redis({
-//     host: "127.0.0.1",
-//     port: "6379"
-// });
-
-const IoRedis = (await import("ioredis")).default;
-redisClient = new IoRedis({
-    host: "127.0.0.1",
-    port: "6379"
-})
-}
-
-(async () => {
-    try{
-        await redisClient.set("test", "connected");
-        const value = await redisClient.get("test");
-        console.log("Redis connected successfully:", value)
-    } catch (error) {
-        console.error("Error connecting to Redis", error);
-    }
-})();
-
-export default redisClient;
+export default redis;
