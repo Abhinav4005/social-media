be36f5cf3aa2c3c9639bd8390d2c9a1a56813394
abhinav <abhinavpathak9984@gmail.com>
2026-02-21 11:30:28 +0530

    fix: remove keyboard text and improved check in useDebounce

--- frontend/src/utils/useDebounce.js
+++ frontend/src/utils/useDebounce.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 
-const useDebounce = (value, delay) => {
+const useDebounce = (value, delay=300) => {
     const [debouncedValue, setDebouncedValue] = useState(value);
 
     useEffect(() => {
+        if(typeof delay != 'function'){
+            return;
+        }
         const handler = setTimeout(() => setDebouncedValue(value), delay);
 
         return () => clearTimeout(handler)
