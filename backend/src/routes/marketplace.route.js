bec84834962416de3f1bc0af0eaa2a4fff2bd036
abhinav <abhinavpathak9984@gmail.com>
2026-08-23 23:43:59 +0530

    feat: update the authentication middleware

--- backend/src/routes/marketplace.route.js
+++ backend/src/routes/marketplace.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { authenticateToken } from "../middleware/authenticateToken.js";
+import authenticateToken from "../middleware/authenticateToken.js";
 import {
     createListing,
     getListings,
